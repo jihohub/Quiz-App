3521784089beb41920fadaf6943d2ea4cb5ce4a7
Jiho Park <diorpark@gmail.com>
2022-09-01 21:31:13 +0900

    feat: sortChoices 유틸 함수가 정답의 index도 함께 return하도록 수정

--- src/components/utils/sortChoices.test.ts
+++ src/components/utils/sortChoices.test.ts
@@ -1,43 +1,38 @@
 const sortChoices = require("./sortChoices");
 
 test("add 200 to [1000, 4, 30]", () => {
-  expect(sortChoices([1000, 4, 30], 200)).toStrictEqual([4, 30, 200, 1000]);
+  expect(sortChoices([1000, 4, 30], 200)).toStrictEqual([
+    [4, 30, 200, 1000],
+    2,
+  ]);
 });
 
 test("add two hundred to [1000, 4, 30]", () => {
   expect(sortChoices([1000, 4, 30], "two hundred")).toStrictEqual([
-    4,
-    30,
-    1000,
-    "two hundred",
+    [4, 30, 1000, "two hundred"],
+    3,
   ]);
 });
 
 test("add 200 to [1000, four, 30]", () => {
   expect(sortChoices([1000, "four", 30], 200)).toStrictEqual([
-    30,
-    200,
-    1000,
-    "four",
+    [30, 200, 1000, "four"],
+    1,
   ]);
 });
 
 test("add two hundred to [1000, four, 30]", () => {
   expect(sortChoices([1000, "four", 30], "two hundred")).toStrictEqual([
-    30,
-    1000,
-    "four",
-    "two hundred",
+    [30, 1000, "four", "two hundred"],
+    3,
   ]);
 });
 
 test("add 200 to [one thousand, four, thirty]", () => {
   expect(sortChoices(["one thousand", "four", "thirty"], 200)).toStrictEqual([
-    200,
-    "four",
-    "one thousand",
-    "thirty",
+    [200, "four", "one thousand", "thirty"],
+    0,
   ]);
 });
 
-export { };
+export {};
--- src/components/utils/sortChoices.ts
+++ src/components/utils/sortChoices.ts
@@ -1,7 +1,7 @@
 export const sortChoices = (
   incorrectAnswers: Array<string | number>,
   correctAnswer: string | number
-): Array<string | number> => {
+): [Array<string | number>, string | number] => {
   const choices = incorrectAnswers.filter(() => true);
   const sorter = (a: number | string, b: number | string): any => {
     if (typeof a === "number" && typeof b === "string") {
@@ -19,7 +19,7 @@ export const sortChoices = (
   };
   choices.push(correctAnswer);
   choices.sort(sorter);
-  return choices;
+  return [choices, choices.indexOf(correctAnswer)];
 };
 
 module.exports = sortChoices;
